7ee00fbb10e4d7e7c633c5359f8201855633caec
PedroBettencourt <pedropbettencourt@gmail.com>
2025-05-23 16:43:34 +0000

    Add start of front-end with react-router

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      Blog!
+    </>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css'
+import App from './App.jsx'
+import Posts from './Posts.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/posts",
+    element: <Posts />
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
