05ac6cc91e82608b90274d97f9fc4655d3c8a6b7
PedroBettencourt <pedropbettencourt@gmail.com>
2025-06-17 22:06:46 +0000

    Update user profile

--- client/src/Profile/Profile.jsx
+++ client/src/Profile/Profile.jsx
@@ -13,7 +13,7 @@ function Profile() {
         async function fetchData() {
             setIsLoading(true);
             try {
-                const res = await fetch(`http://localhost:3000/users/${userId}`, { headers: { "Authorization": "Bearer " + localStorage.token } });
+                const res = await fetch(`http://localhost:3000/users/${userId}`);
                 const json = await res.json();
                 setUser(json);
             } catch (err) {
@@ -35,16 +35,24 @@ function Profile() {
             { user && <h1>{user.username}'s Profile</h1> }
             
             <div className={ postsClass }>
-                { user && user.posts && user.posts.map(post => (
+                { user && (user.posts.length !== 0) && <h2>Posts</h2> }
+                { user && user.posts && user.posts.map(post =>
                     <ul key={post.id}>
                         <li>{post.title}</li>
                         <li>{new Date(post.createdAt).toUTCString()}</li>
                         <li>{post.text}</li>
-                        {(user.id === 6) && <li>hi</li>}
                     </ul>
-                ))}
+                ) }
+
+                { user && (user.comments.length !== 0) && <h2>Comments</h2> }
+                { user && user.comments && user.comments.map(comment =>
+                    <ul key={comment.id}>
+                        <li>{comment.author.username}</li>
+                        <li>{new Date(comment.createdAt).toUTCString()}</li>
+                        <li>{comment.text}</li>
+                    </ul>
+                ) }
             </div>
-            {console.log(user)}
         </div>
     );
 };
